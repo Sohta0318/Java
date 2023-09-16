518aac7fe6a4377db2aa5091ea844a5a762a0e7b
SOHTA0318 <nicosohta0318@gmail.com>
2023-09-17 00:59:42 +0900

    lec 198 setup

--- sec15 Collections/CollectionsMethods/src/dev/lpa/Main.java
+++ sec15 Collections/CollectionsMethods/src/dev/lpa/Main.java
@@ -1,33 +1,10 @@
 package dev.lpa;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
-    }
-}
-class Card{
-    private Suit suit;
-    private String face;
-    private int rank;
-
-    public Card(Suit suit, String face, int rank) {
-        this.suit = suit;
-        this.face = face;
-        this.rank = rank;
-    }
-
-    @Override
-    public String toString() {
-        return "Card{" +
-                "suit=" + suit +
-                ", face='" + face + '\'' +
-                ", rank=" + rank +
-                '}';
-    }
-    public static Card getNumericCard(Suit suit, int number){
-        return new Card();
-    }
-    public static Card getFaceCars(Suit suit, String face){
-
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
     }
 }
--- sec15 Collections/CollectionsMethods/src/dev/lpa/Suit.java
+++
@@ -1,5 +0,0 @@
-package dev.lpa;
-
-public enum Suit {
-    CLUB, DIAMOND, HEART, SPADE
-}
