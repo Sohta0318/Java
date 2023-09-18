89e930e9ee9f0ae76ba5cf8e41921f680bb5b5c8
SOHTA0318 <nicosohta0318@gmail.com>
2023-09-18 23:14:56 +0900

    lec 201 Collections methods

--- sec15 Collections/CollectionsMethods/src/dev/lpa/Main.java
+++ sec15 Collections/CollectionsMethods/src/dev/lpa/Main.java
@@ -36,6 +36,89 @@ Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
         cards = List.copyOf(kingsOfClubs);
         Card.printDeck(cards, "List copy of kings", 1);
 
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
 
+        Collections.shuffle(deck);
+        Card.printDeck(deck, "Shuffled Deck", 4);
+
+        Collections.reverse(deck);
+        Card.printDeck(deck, "Reversed Deck of Cards", 4);
+
+        var sortingAlgorithm = Comparator.comparing(Card::rank)
+                .thenComparing(Card::suit);
+        Collections.sort(deck, sortingAlgorithm);
+        Card.printDeck(deck, "Standard Deck sorted by rank, suit", 13);
+
+        Collections.reverse(deck);
+        Card.printDeck(deck, "Standard Deck reversed", 13);
+
+        List<Card> kings = new ArrayList<>(deck.subList(4,8));
+        Card.printDeck(kings, "Kings in deck", 1);
+
+        List<Card> tens = new ArrayList<>(deck.subList(16,20));
+        Card.printDeck(tens, "Tens in deck", 1);
+
+//        Collections.shuffle(deck);
+        int sublistIndex = Collections.indexOfSubList(deck, tens);
+        System.out.println("Sublist index for tens = " + sublistIndex);
+        System.out.println("Contains = " + deck.containsAll(tens));
+
+        boolean disjoint = Collections.disjoint(deck, tens);
+        System.out.println("disjoint = " + disjoint);
+
+        boolean disjoint2 = Collections.disjoint(kings, tens);
+        System.out.println("disjoint = " + disjoint2);
+
+//        Collections.sort(deck, sortingAlgorithm);
+        deck.sort(sortingAlgorithm);
+        Card tenOfHearts = Card.getNumericCard(Card.Suit.HEART, 10);
+        int foundIndex = Collections.binarySearch(deck, tenOfHearts, sortingAlgorithm);
+        System.out.println("FoundIndex = " + foundIndex);
+        System.out.println("FoundIndex = " + deck.indexOf(tenOfHearts));
+        System.out.println(deck.get(foundIndex));
+
+        Card tenOfClubs = Card.getNumericCard(Card.Suit.CLUB, 10);
+        Collections.replaceAll(deck, tenOfClubs, tenOfHearts);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        Collections.replaceAll(deck, tenOfHearts, tenOfClubs);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        if(Collections.replaceAll(deck, tenOfHearts, tenOfClubs)){
+            System.out.println("Tens of Hearts replaced with tens of Clubs");
+        }else {
+            System.out.println("No tens of Hearts found in the list");
+        }
+
+        System.out.println("Ten of Clubs Cards = " +
+                Collections.frequency(deck, tenOfClubs));
+System.out.println("Best Card = " + Collections.max(deck, sortingAlgorithm));
+        System.out.println("Worst Card = " + Collections.min(deck, sortingAlgorithm));
+
+        var sortBySuit = Comparator.comparing(Card::suit)
+                .thenComparing(Card::rank);
+        deck.sort(sortBySuit);
+        Card.printDeck(deck, "Sorted by Suit, Rank", 4);
+
+        List<Card> copied = new ArrayList<>(deck.subList(0,13));
+        Collections.rotate(copied,2);
+        System.out.println("UnRotated " + deck.subList(0,13));
+        System.out.println("Rotated " + 2 + ": " + copied);
+
+        copied = new ArrayList<>(deck.subList(0,13));
+        Collections.rotate(copied,-2);
+        System.out.println("UnRotated " + deck.subList(0,13));
+        System.out.println("Rotated " + -2 + ": " + copied);
+
+        copied = new ArrayList<>(deck.subList(0,13));
+        for(int i = 0; i < copied.size(); i++){
+            Collections.swap(copied, i, copied.size() -1 - i);
+        }
+System.out.println("Manual reverse :" + copied);
+
+        copied = new ArrayList<>(deck.subList(0,13));
+        Collections.reverse(copied);
+        System.out.println("Using reverse :" + copied);
     }
 }
