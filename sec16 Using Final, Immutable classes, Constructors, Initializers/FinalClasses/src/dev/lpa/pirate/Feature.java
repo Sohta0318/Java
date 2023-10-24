bbf4e0ec3329eb2311be5979e89e5e28262d34dd
SOHTA0318 <nicosohta0318@gmail.com>
2023-10-24 17:35:48 +0900

    lec final section

---
+++ sec16 Using Final, Immutable classes, Constructors, Initializers/FinalClasses/src/dev/lpa/pirate/Feature.java
@@ -0,0 +1,21 @@
+package dev.lpa.pirate;
+
+public enum Feature {
+    ALLIGATOR(-45),
+    ALOE(5),
+    JELLYFISH(-10),
+    PINEAPPLE(10),
+    SNAKE(-25),
+    SPRING(25),
+    SUN_POISON(-15);
+
+    private final int healthPoint;
+
+    Feature(int healthPoint) {
+        this.healthPoint = healthPoint;
+    }
+
+    public int getHealthPoint() {
+        return healthPoint;
+    }
+}
