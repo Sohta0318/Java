ae6b3ef1e3535feedb9fb462e0d47ea3b7d66dff
SOHTA0318 <nicosohta0318@gmail.com>
2023-11-07 20:37:30 +0900

    lec 252 intermediate stream operation

---
+++ sec17 Streams/StreamsIntermediate/src/dev/lpa/Main.java
@@ -0,0 +1,28 @@
+package dev.lpa;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        IntStream.iterate((int) 'A', i-> i <= (int) 'z', i -> i +1)
+                .filter(Character::isAlphabetic)
+                .map(Character::toUpperCase)
+                .distinct()
+//                .dropWhile(i -> Character.toUpperCase(i) <= 'E')
+//                .takeWhile(i -> i > 'a')
+//                .skip(5)
+//                .filter(c -> Character.toUpperCase(c) > 'E')
+                .forEach(d -> System.out.printf("%c", d));
+
+        System.out.println();
+        Random random = new Random();
+
+        Stream.generate(() -> random.nextInt((int) 'A', (int) 'z' + 1))
+                .limit(50)
+                .distinct()
+                .sorted()
+                .forEach(d -> System.out.printf("%c", d));
+    }
+}
