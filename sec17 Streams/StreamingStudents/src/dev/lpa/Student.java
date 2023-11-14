8e735c790a71272214f9b16f2e81eef94bc5e6bb
SOHTA0318 <nicosohta0318@gmail.com>
2023-11-14 18:07:08 +0900

    sec 17 stream

--- sec17 Streams/StreamingStudents/src/dev/lpa/Student.java
+++ sec17 Streams/StreamingStudents/src/dev/lpa/Student.java
@@ -1,9 +1,7 @@
 package dev.lpa;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 public class Student {
     private static long lastStudentId = 1;
@@ -99,16 +97,25 @@ public class Student {
         return data[random.nextInt(data.length)];
     }
 
+    private static Course[] getRandomSelection(Course ...courses){
+        int courseCount = random.nextInt(1, courses.length + 1);
+        List<Course> courseList = new ArrayList<>(Arrays.asList(courses));
+        Collections.shuffle(courseList);
+        List<Course> selectedCourses = courseList.subList(0, courseCount);
+        return selectedCourses.toArray(new Course[0]);
+    }
+
     public static Student getRandomStudent(Course... courses){
         int maxYear = LocalDate.now().getYear() + 1;
+        Course[] randomCourses = getRandomSelection(courses);
         Student student = new Student(
                 getRandomVal("AU", "CA", "CN", "GB", "IN", "UA", "US"),
                 random.nextInt(2015, maxYear),
                 random.nextInt(18, 90),
                 getRandomVal("M", "F", "U"),
-                random.nextBoolean(), courses
+                random.nextBoolean(), randomCourses
         );
-        for (Course course: courses){
+        for (Course course: randomCourses){
             int lecture = random.nextInt(30, course.lectureCount());
             int year = random.nextInt(student.getYearEnrolled(), maxYear);
             int month = random.nextInt(1, 13);
