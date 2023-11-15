4897335f05a8f75d0042f8ced7d7f67a415e8217
SOHTA0318 <nicosohta0318@gmail.com>
2023-11-15 11:49:15 +0900

    lec 269 Randomazation

--- sec18 Back to Nuts and Bolts/MathRandomProject/src/dev/lpa/Main.java
+++ sec18 Back to Nuts and Bolts/MathRandomProject/src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 //        int maxMinusFive = Integer.MAX_VALUE -5;
@@ -10,7 +12,7 @@ public class Main {
         System.out.println(Math.abs(-50));
         System.out.println(Math.abs(Integer.MIN_VALUE));
 //        System.out.println(Math.absExact(Integer.MIN_VALUE));
-        System.out.println(Math.absExact((long)Integer.MIN_VALUE));
+        System.out.println(Math.absExact((long) Integer.MIN_VALUE));
 
         System.out.println("Max = " + Math.max(10, -10));
         System.out.println("Min = " + Math.min(10.000002f, 10.001f));
@@ -26,8 +28,55 @@ public class Main {
         System.out.println("2 to the third power (2*2*2) = " + Math.pow(2, 3));
         System.out.println("10 to the fifth power (10*10*10*10*10) = " + Math.pow(10, 5));
 
-for(int i = 0; i < 10; i++){
-    System.out.println(Math.random());
-}
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%1$d = %1$c%n", (int) (Math.random() * 26) + 65);
+        }
+
+        System.out.println("-".repeat(30));
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%1$d = %1$c%n", r.nextInt(65, 91));
+        }
+
+        System.out.println("-".repeat(30));
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%1$d = %1$c%n",
+                    r.nextInt((int) 'A', (int) 'Z' + 1 ));
+        }
+
+        System.out.println("-".repeat(30));
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%1$d%n",
+                    r.nextInt(-10, 11));
+        }
+
+        System.out.println("-".repeat(30));
+        r.ints()
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("-".repeat(30));
+        r.ints(0, 10)
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("-".repeat(30));
+        r.ints(10, 0, 10)
+                .forEach(System.out::println);
+
+        System.out.println("-".repeat(30));
+        r.ints(10)
+                .forEach(System.out::println);
+
+        long nanoTime = System.nanoTime();
+        Random pseudoRandom = new Random(nanoTime);
+        System.out.println("-".repeat(30));
+        pseudoRandom.ints(10)
+                .forEach(i -> System.out.println(i + " "));
+
+        Random notReallyRandom = new Random(nanoTime);
+        System.out.println("\n" + "-".repeat(30));
+        notReallyRandom.ints(10)
+                .forEach(i -> System.out.println(i + " "));
     }
 }
