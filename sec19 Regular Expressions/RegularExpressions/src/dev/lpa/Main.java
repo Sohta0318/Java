270d90ea223bf44cf91a89ef6432a0916b9941ec
SOHTA0318 <nicosohta0318@gmail.com>
2023-11-22 13:28:50 +0900

    lec 288 regular expressions methods

--- sec19 Regular Expressions/RegularExpressions/src/dev/lpa/Main.java
+++ sec19 Regular Expressions/RegularExpressions/src/dev/lpa/Main.java
@@ -1,5 +1,8 @@
 package dev.lpa;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String helloWorld = "%s %s".formatted("Hello", "World");
@@ -14,13 +17,52 @@ public class Main {
         String replacement = "(-)";
 
         String[] patterns ={
-                "[abc]","[123]","[A]"
+                "[abc]*$","[123]","^[A]"
         };
         for(String pattern : patterns){
             String output = testString.replaceFirst(pattern, replacement);
             System.out.println("Pattern: " + pattern + " => " + output);
         }
 
+        // Song of the Witches in MacBeth, a Play by Shakespeare
+        String paragraph = """
+                Double, double toil and trouble;
+                Fire burn and caldron bubble.
+                Fillet of a fenny snake,
+                In the caldron boil and bake
+                Eye of newt and toe of frog,
+                Wool of bat and tongue of dog,
+                Adder's fork and blind-worm's sting,
+                Lizard's leg and howlet's wing,
+                For a charm of powerful trouble,
+                Like a hell-broth boil and bubble.
+                """;
+
+        String[] lines = paragraph.split("\\R");
+        System.out.println("This paragraph has " + lines.length + " lines");
+        String[] words = paragraph.split("\\s");
+        System.out.println("This paragraph has " + words.length + " words");
+        System.out.println(paragraph.replaceAll("[a-zA-z]+ble", "[GRUB]"));
+
+        Scanner scanner = new Scanner(paragraph);
+        System.out.println(scanner.delimiter());
+        scanner.useDelimiter("\\R");
+
+//        while (scanner.hasNext()){
+//            String element = scanner.next();
+//            System.out.println(element);
+//        }
+
+//        scanner.tokens()
+//                .map(s -> s.replaceAll("\\p{Punct}", ""))
+//                .flatMap(s -> Arrays.stream(s.split("\\s+")))
+//                .filter(s -> s.matches("[a-zA-z]+ble"))
+//                        .forEach(System.out::println);
+
+        System.out.println(scanner.findInLine("[a-zA-z]+ble"));
+        System.out.println(scanner.findInLine("[a-zA-z]+ble"));
+
+        scanner.close();
     }
 
     private static String format(String regexp, String... args){
