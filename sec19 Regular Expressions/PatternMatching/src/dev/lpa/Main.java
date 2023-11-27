2ba097860397c2c659437a72ea994c792dd966f1
SOHTA0318 <nicosohta0318@gmail.com>
2023-11-27 17:30:34 +0900

    lec 293 HTML tag

--- sec19 Regular Expressions/PatternMatching/src/dev/lpa/Main.java
+++ sec19 Regular Expressions/PatternMatching/src/dev/lpa/Main.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -21,6 +22,71 @@ public class Main {
         System.out.println(matcher.find() + ": " + sentence);
         System.out.println("Matched Ending index " + matcher.end());
         System.out.println("Matched on : " + sentence.substring(matcher.start(), matcher.end()));
+        System.out.println("Matched on : " + matcher.group());
+
+        String htmlSnippet = """
+                <H1>My Heading</H1>
+                <h2>Sub-heading</h2>
+                <p>This is paragraph about something.</p>
+                <p>This is another paragraph about something else.</p>
+                <h3>Summary</h3>
+                """;
+
+        Pattern htmlPattern = Pattern.compile("<[hH](?<level>\\d)>(.*)</[hH]\\d>");
+        Matcher htmlMatcher = htmlPattern.matcher(htmlSnippet);
+
+        while (htmlMatcher.find()) {
+//            System.out.println("Group: " + htmlMatcher.group());
+//            System.out.println("Group0: " + htmlMatcher.group(0));
+//            System.out.println("Group1: " + htmlMatcher.group(1));
+
+            System.out.println(htmlMatcher.group("level") + " " + htmlMatcher.group(2));
+            System.out.println("index = " + htmlMatcher.start("level"));
+
+        }
+        htmlMatcher.reset();
+        htmlMatcher.results().forEach(mr -> System.out.println(mr.group() + " " + mr.group(2)));
+
+        String tabbedText = """
+                group1  group2  group3
+                1   2   3
+                a   b   d
+                """;
+
+        tabbedText.lines().flatMap(
+                s -> Pattern.compile("\\t").splitAsStream(s)).forEach(System.out::println);
+
+        htmlMatcher.reset();
+        String updatedSnippet = htmlMatcher.replaceFirst((mr) -> "<em>" + mr.group(2) + "</em>");
+        System.out.println("-".repeat(30));
+        System.out.println(updatedSnippet);
+        System.out.println(htmlMatcher.start() + " : " + htmlMatcher.end());
+        System.out.println(htmlMatcher.group(2));
+
+        htmlMatcher.usePattern(Pattern.compile("<([hH]\\d)>(.*)</\\1>"));
+        htmlMatcher.reset();
+        System.out.println("-".repeat(30));
+        System.out.println("Using Back Reference: \n" +
+                htmlMatcher.replaceFirst("<em>$2</em>"));
+
+        String replaceHTML = htmlMatcher.replaceAll((mr) -> "<em>" + mr.group(2) + "</em>");
+        System.out.println("-".repeat(30));
+        System.out.println(replaceHTML);
+
+        htmlMatcher.reset();
+        StringBuilder sb = new StringBuilder();
+        int index = 1;
+        while (htmlMatcher.find()) {
+            htmlMatcher.appendReplacement(sb,
+                    switch (htmlMatcher.group(1).toLowerCase()) {
+                        case "h1" -> "<head>$2</head>";
+                        case "h2" -> "<em>$2</em>";
+                        default -> "<$1>" + index++ + ". $2</$1>";
+                    }
+            );
+        }
+        htmlMatcher.appendTail(sb);
+        System.out.println(sb);
 
     }
 }
