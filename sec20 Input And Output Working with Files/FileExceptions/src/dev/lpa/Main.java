5efee257186ef205518107b7d33c11d97476343c
SOHTA0318 <nicosohta0318@gmail.com>
2023-11-28 13:01:25 +0900

    lec 296 Exception Check: Uncheck vs Checked

---
+++ sec20 Input And Output Working with Files/FileExceptions/src/dev/lpa/Main.java
@@ -0,0 +1,36 @@
+package dev.lpa;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String filename = "testing.csv";
+
+        testFile(filename);
+        File file = new File(filename);
+        if(!file.exists()){
+            System.out.println("I can't run unless this file exist");
+            System.out.println("Quitting Application, go figure it out");
+            return;
+        }
+        System.out.println("I'm good to go.");
+    }
+    private static void testFile(String filename) throws IOException {
+        Path path = Paths.get(filename);
+        try{
+            List<String> lines = Files.readAllLines(path);
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }finally {
+            System.out.println("Maybe I'd log something either way...");
+        }
+        System.out.println("File exists and able to use as resource!");
+
+    }
+
+}
