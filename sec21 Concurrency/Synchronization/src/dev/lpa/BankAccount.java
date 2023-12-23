e1ab32b7663aff7b6557470b0b61c6184fc11f0f
SOHTA0318 <nicosohta0318@gmail.com>
2023-12-23 15:40:27 +0900

    lec 328 Synchronization block

---
+++ sec21 Concurrency/Synchronization/src/dev/lpa/BankAccount.java
@@ -0,0 +1,79 @@
+package dev.lpa;
+
+public class BankAccount {
+    private double balance;
+    private String name;
+    private final Object lockName = new Object();
+    private final Object lockBalance = new Object();
+
+    public BankAccount(String name ,double balance) {
+        this.balance = balance;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        synchronized (lockName) {
+            this.name = name;
+            System.out.println("Updated name = " + name);
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public  void deposit(double amount){
+
+        try{
+            System.out.println("Deposit - Talking to the teller at the bank ...");
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        synchronized(lockBalance) {
+            double originalBalance = balance;
+            balance += amount;
+            System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f)" +
+                    " :NEW BALANCE = %.0f%n", originalBalance, amount, balance);
+            addPromoDollars(amount);
+        }
+    }
+
+    private void addPromoDollars(double amount){
+
+        if(amount >= 5000){
+            synchronized (lockBalance){
+                System.out.println("Congratulations, you earned a promotional deposit.");
+                balance += 25;
+            }
+        }
+    }
+
+    public synchronized void withdraw(double amount){
+
+        try{
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        double originalBalance = balance;
+
+        if(amount <= balance) {
+            balance -= amount;
+            System.out.printf("STARTING BALANCE: %.0f, WITHDRAW (%.0f)" +
+                    " :NEW BALANCE = %.0f%n", originalBalance, amount, balance);
+        }else {
+            System.out.printf("STARTING BALANCE: %.0f, WITHDRAW (%.0f)" +
+                    " :INSUFFICIENT FUNDS!", originalBalance, amount);
+        }
+    }
+
+
+}
