9c19af0ccd0858fb97f93158c95487c8f6ae6da2
SOHTA0318 <nicosohta0318@gmail.com>
2023-12-25 13:48:05 +0900

    lec 333 ReetrantLock

--- sec21 Concurrency/ConsummerProducer/src/dev/lpa/Main.java
+++ sec21 Concurrency/ConsummerProducer/src/dev/lpa/Main.java
@@ -1,24 +1,58 @@
 package dev.lpa;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class MessageRepository{
     private String message;
     private boolean hasMessage = false;
+    private final Lock lock = new ReentrantLock();
 
-    public synchronized String read(){
-        while(!hasMessage){
-
+    public  String read(){
+        if(lock.tryLock()) {
+            try {
+                while (!hasMessage) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                hasMessage = false;
+            } finally {
+                lock.unlock();
+            }
+        }else {
+            System.out.println("** read blocked " + lock);
+            hasMessage = false;
         }
-        hasMessage = false;
         return message;
     }
 
-    public synchronized  void write(String message){
-        while(hasMessage){
-
+    public   void write(String message){
+        try {
+            if (lock.tryLock(3, TimeUnit.SECONDS)) {
+                try {
+                    while (hasMessage) {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    hasMessage = true;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("** write blocked " + lock);
+                hasMessage = true;
+            }
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
         }
-        hasMessage = true;
         this.message = message;
     }
 }
@@ -40,15 +74,16 @@ class MessageWriter implements Runnable{
     public void run() {
         Random random = new Random();
         String[] lines = text.split("\n");
-        for(int i = 0; i < lines.length; i++){
-            outgoingMessage.write(lines[i]);
-            try{
-                Thread.sleep(random.nextInt(500,2000));
+        for (String line : lines) {
+            outgoingMessage.write(line);
+            try {
+                Thread.sleep(random.nextInt(500, 2000));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            outgoingMessage.write("Finished");
         }
+        outgoingMessage.write("Finished");
+
     }
 }
 
@@ -78,9 +113,24 @@ do{
 public class Main {
     public static void main(String[] args) {
 MessageRepository messageRepository = new MessageRepository();
-Thread reader = new Thread(new MessageReader(messageRepository));
-Thread writer = new Thread(new MessageWriter(messageRepository));
+Thread reader = new Thread(new MessageReader(messageRepository), "Reader");
+Thread writer = new Thread(new MessageWriter(messageRepository), "Writer");
 
+writer.setUncaughtExceptionHandler((thread, exc) -> {
+    System.out.println("Writer had exception: " + exc);
+    if(reader.isAlive()){
+        System.out.println("Going to interrupt the reader");
+        reader.interrupt();
+    }
+});
+
+        reader.setUncaughtExceptionHandler((thread, exc) -> {
+            System.out.println("Reader had exception: " + exc);
+            if(writer.isAlive()){
+                System.out.println("Going to interrupt the writer");
+                writer.interrupt();
+            }
+        });
 reader.start();
 writer.start();
 
