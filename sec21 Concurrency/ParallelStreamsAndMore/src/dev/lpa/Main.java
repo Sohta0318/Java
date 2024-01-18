e64c0e1d600f26479af76a7fde7bbdc958d0cafb
SOHTA0318 <nicosohta0318@gmail.com>
2024-01-18 17:50:53 +0900

    sec 342 Synchronized & Concurrent Map

--- sec21 Concurrency/ParallelStreamsAndMore/src/dev/lpa/Main.java
+++ sec21 Concurrency/ParallelStreamsAndMore/src/dev/lpa/Main.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -68,7 +69,7 @@ public class Main {
                 Stream.generate(Person::new)
                         .limit(10000)
                         .parallel()
-                        .collect(Collectors.groupingBy(
+                        .collect(Collectors.groupingByConcurrent(
                                 Person::lastName,
                                 Collectors.counting()
                         ));
@@ -79,5 +80,24 @@ public class Main {
             total += count;
         }
         System.out.println("Total = " + total);
+
+        System.out.println(lastNameCounts.getClass().getName());
+
+        var lastCounts = Collections.synchronizedMap(new TreeMap<String, Long>());
+        Stream.generate(Person::new)
+                .limit(10000)
+                .parallel()
+                .forEach((person) -> lastCounts.merge(person.lastName()
+                ,1L, Long::sum));
+
+        System.out.println(lastCounts);
+
+         total = 0;
+        for(long count : lastCounts.values()){
+            total += count;
+        }
+        System.out.println("Total = " + total);
+
+        System.out.println(lastCounts.getClass().getName());
     }
 }
