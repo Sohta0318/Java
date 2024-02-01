73fd364cb48a2247433f237e0f37ad545946d558
SOHTA0318 <nicosohta0318@gmail.com>
2024-02-01 11:12:23 +0900

    lec 435 music sqlite database

---
+++ sec25 Databases/Music/src/dev/lpa/Main.java
@@ -0,0 +1,14 @@
+package dev.lpa;
+
+import dev.lpa.model.Datasource;
+
+public class Main {
+    public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if(!datasource.open()){
+            System.out.println("Can't open datasource");
+            return;
+        }
+        datasource.close();
+    }
+}
