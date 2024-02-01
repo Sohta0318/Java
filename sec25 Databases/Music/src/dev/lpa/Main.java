df05d44f9b9cd712a13c976b80c0fbc2f5b0983c
SOHTA0318 <nicosohta0318@gmail.com>
2024-02-01 19:12:52 +0900

    lec 438 Query albums by method

--- sec25 Databases/Music/src/dev/lpa/Main.java
+++ sec25 Databases/Music/src/dev/lpa/Main.java
@@ -13,7 +13,7 @@ public class Main {
             return;
         }
 
-        List<Artist> artists = datasource.queryArtists();
+        List<Artist> artists = datasource.queryArtists(Datasource.ORDER_BY_ASC);
         if (artists == null) {
             System.out.println("No Artists!");
             return;
@@ -22,6 +22,13 @@ public class Main {
         for(Artist artist : artists){
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
+
+        List<String> albumsForArtists = datasource.queryAlbumsForArtist("Iron Maiden", Datasource.ORDER_BY_ASC);
+
+        for(String album : albumsForArtists){
+            System.out.println(album);
+        }
+
         datasource.close();
     }
 }
