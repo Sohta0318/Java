8d5511ac05cb1f51a6f138a5fd50bea30e15810b
SOHTA0318 <nicosohta0318@gmail.com>
2024-02-03 10:13:48 +0900

    lec 455 Socker Server & Client

---
+++ sec26 Networking Programming/EchoClient/src/dev/lpa/Main.java
@@ -0,0 +1,35 @@
+package dev.lpa;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try(Socket socket = new Socket("localhost", 8000)){
+            BufferedReader echoes = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter stringToEchoes = new PrintWriter(socket.getOutputStream(), true);
+
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+            String response;
+
+            do{
+                System.out.println("Enter string to be echoed: ");
+                echoString = scanner.nextLine();
+                stringToEchoes.println(echoString);
+                if(!echoString.equals("exit")){
+                    response = echoes.readLine();
+                    System.out.println(response);
+                }
+            }while(!echoString.equals("exit"));
+
+        }catch (IOException e){
+            System.out.println("Client Error: " + e.getMessage());
+        }
+    }
+}
